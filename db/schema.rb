8bbe7c0dc920aead09a31f453e033cd20f47f33b
Victor Cortes <ie.cortex@gmail.com>
2017-02-17 16:15:11 -0500

    Add picture to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217202314) do
+ActiveRecord::Schema.define(version: 20170217210616) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer  "patient_id"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20170217202314) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "disease_id"
+    t.string   "picture"
   end
 
   create_table "users", force: :cascade do |t|
